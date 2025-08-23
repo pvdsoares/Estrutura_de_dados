032e330256229be0e2bc50a7cdb52a27015b9581
paulo_soares <202421190003@ifba.edu.br>
2025-08-23 17:43:11 -0300

    Resolucao questão 1 array

---
+++ Questao1.java
@@ -0,0 +1,12 @@
+//Crie um algoritmo/fluxograma/programa que declare um array de inteiros e imprima seus elementos.
+public class Questao1 {
+    public static void main(String[] args) {
+      int[] array= {10, 20, 30, 40};
+        
+        for(int i=0; i< array.length; i++) {
+            System.out.println("posição: "+ i + " nº: "+ array[i]);
+       }
+    }
+}
+
+
